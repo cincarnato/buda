2c485b24ec56171ea6b50ef57aa6d4f4c99c3f55
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2018-02-26 18:47:08 -0300

    Responsive

--- module/Landing/config/module.config.php
+++ module/Landing/config/module.config.php
@@ -2,7 +2,7 @@
 
 $setting = array_merge_recursive(
 include "controller.config.php",
-include "doctrine.config.php",
+//include "doctrine.config.php",
 include "host-route.config.php",
 include "route.config.php",
 include "view.config.php"
