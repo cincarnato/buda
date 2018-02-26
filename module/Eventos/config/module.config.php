b237b867d0ec5b3c7326fff2c2dbdff57e53cf97
crist <cristian.cdi@gmail.com>
2018-02-26 00:23:25 -0300

    Init Eventos

--- module/Eventos/config/module.config.php
+++ module/Eventos/config/module.config.php
@@ -1,3 +1,12 @@
 <?php
 
-return array();
+$setting = array_merge_recursive(
+include "doctrine.config.php",
+include "view.config.php",
+include "controller.config.php",
+include "navigation.config.php",
+include "route.config.php",
+include "zfm-datagrid.lugar.config.php"
+);
+
+return $setting;
