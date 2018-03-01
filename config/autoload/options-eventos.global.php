ef67ece4f7b133def53edc3998c381d9bbf93724
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2018-03-01 15:33:24 -0300

    Facebook Login

--- config/autoload/options-eventos.global.php
+++ config/autoload/options-eventos.global.php
@@ -4,7 +4,7 @@ return [
     'Eventos.options' => [
         "facebook_app_id" => "1614454111970816",
         "facebook_app_secret" => "49bacaeea920b8344bb112b4ccc8b4d1",
-        "facebook_default_graph_version" => "v2.10",
+        "facebook_default_graph_version" => "v2.12",
         "facebook_default_access_token" => "",
     ]
 ];
